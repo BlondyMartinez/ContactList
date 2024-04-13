555db72b611c0dde337bd7e8c1dd66cc9f041eb3
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-13 10:04:25 +0000

    navbar now shows user name

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+    const { store } = useContext(Context);
+    const [username, setUsername] = useState("");
+	const [endsInS, setEndsInS] = useState(false);
+
+	useEffect(() => {
+        setUsername(store.user?.slug || "");
+        
+        setEndsInS(store.user?.slug?.toLowerCase().endsWith("s") || false);
+    }, [store.user])
+
+
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">{`${username}${endsInS ? "'" : "'s"} Agenda`}</span>
 			</Link>
 			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
 			</div>
 		</nav>
 	);
