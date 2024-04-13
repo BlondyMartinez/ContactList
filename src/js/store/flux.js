c8a405c7b7ffb3091281571cdfccb2fcb5110726
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-13 06:25:47 +0000

    added some API functions

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,35 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			users: [],
+			user: {},
+			contacts: [],
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			loadUserList: () => {
+				fetch(`https://playground.4geeks.com/contact/agendas?offset=0&limit=100`)
+				.then(response => { return response.json(); })
+				.then(data => { setStore({ "users": data.users }); })
+				.catch(error => { console.error('Error fetching users:', error); });
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+
+			loadContactList: (username) => {
+				fetch(`https://playground.4geeks.com/contact/agendas/${username}/contacts`)
+				.then(response => { return response.json(); })
+				.then(data => { setStore({ "contacts": data.contacts }); })
+				.catch(error => { console.error('Error fetching contacts:', error); });
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+			createUser: (username) => {
+				const config = { 
+					method: "POST",
+					body: JSON.stringify({ "slug": username }),
+					headers: { "Accept": "application/json" }
+				}
 
-				//reset the global store
-				setStore({ demo: demo });
+				fetch(`https://playground.4geeks.com/contact/agendas/${username}`, config)
+				.then(() => { getActions().loadContactList(username); setStore({ "user": username }); })
+				.catch(error => { console.error('Error fetching contacts:', error); });
 			}
 		}
 	};
