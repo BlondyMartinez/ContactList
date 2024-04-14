9e34144de51d24d8c9f236294ca67b1568a71530
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-14 18:04:48 +0000

    styled home

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,10 +4,12 @@ import "../../styles/home.css";
 import ContactList from "../component/contact_list.jsx"; 
 
 export const Home = () => (
-	<div>
-		<Link to={"/add_contact"}>
-			<button className="btn btn-success">New Contact</button>
-		</Link>
-		<ContactList></ContactList>
+	<div className="w-100 d-flex flex-column align-items-center">
+		<div className="col-sm-11 col-md-8 col-lg-6">
+			<Link to={"/add_contact"}>
+				<button className="btn btn-success mb-2">New Contact</button>
+			</Link>
+			<ContactList></ContactList>
+		</div>
 	</div>
 );
