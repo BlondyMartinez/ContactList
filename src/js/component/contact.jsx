ac4b707e730c406692feb34079efe6a9c02689c1
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-14 18:06:47 +0000

    styled contacts and now contacts can be deleted

--- src/js/component/contact.jsx
+++ src/js/component/contact.jsx
@@ -1,12 +1,67 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { Context } from "../store/appContext";
+import { Icon } from '@iconify/react';
 
 const Contact = (props) => {
+    const { actions } = useContext(Context);
+
+    const [isOpen, setIsOpen] = useState(false);
+    const [onHover, setOnHover] = useState(false);
+
     return (
-        <div className="card">
-            <h6>{props.name}</h6>
-            <p>{props.phone}</p>
-            <p>{props.email}</p>
-            <p>{props.address}</p>
+        <div 
+            className="w-100 bg-white border border-1 rounded" 
+            onMouseEnter={() => { setOnHover(true) }} 
+            onMouseOver={() => { setOnHover(true) }} 
+            onMouseLeave={() => { setOnHover(false) }}
+        >
+            <div className="d-flex align-items-center justify-content-between">   
+                <div className="d-flex align-items-center">
+                    <Icon style={{ fontSize: "6rem" }} icon="ph:user-circle-duotone" /> 
+                    <h3>{props.name}</h3>
+                </div>
+                <div>
+                    { onHover &&
+                        <button className="btn">
+                            <Icon className="fs-5 text-primary" icon="material-symbols:edit-square-outline" />
+                        </button>
+                    }
+                    { onHover &&
+                        <button className="btn" onClick={() => { actions.deleteContact(props.id) }}>
+                            <Icon className="fs-5 text-danger" icon="material-symbols:delete-outline" />
+                        </button>
+                    }
+                    <button className="btn me-3" onClick={() => {setIsOpen(!isOpen)}}>
+                        {isOpen 
+                            ? <Icon className="fs-3" icon="material-symbols:arrow-drop-up" /> 
+                            : <Icon className="fs-3" icon="material-symbols:arrow-drop-down" />
+                        }
+                    </button>
+                </div>
+            </div>
+
+            {isOpen && 
+                <div className="d-flex flex-column ms-3 mb-3">
+                    {props.phone !== "" &&
+                        <div className="d-flex align-items-center">
+                            <Icon className="fs-6 me-1" icon="material-symbols:phone-android" />
+                            <span>{props.phone}</span>
+                        </div>
+                    }
+                    {props.email !== "" &&
+                        <div className="d-flex align-items-center">
+                            <Icon className="fs-6 me-1" icon="mdi:email" />
+                            <span>{props.email}</span>
+                        </div>
+                    }
+                    {props.address !== "" &&
+                        <div className="d-flex align-items-center">
+                            <Icon className="fs-6 me-1" icon="bi:house-fill" />
+                            <span>{props.address}</span>
+                        </div>
+                    }
+                </div>
+            }
         </div>
     )
 }
