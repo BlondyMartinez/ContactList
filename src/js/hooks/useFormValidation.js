4825179db8349e14f9ba53c81b9ecb6b232baffd
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-14 13:44:33 +0000

    form is now reset after saving successfully

--- src/js/hooks/useFormValidation.js
+++ src/js/hooks/useFormValidation.js
@@ -7,6 +7,28 @@ const useFormValidation = () => {
     const [address, setAddress] = useState("");
     const [isEmailValid, setIsEmailValid] = useState(true);
     const [isValid, setIsValid] = useState(false);
+    const [wasValidated, setWasValidated] = useState(false);
+
+    
+    useEffect(() => {
+        const initializeValidation = () => {
+            const forms = document.querySelectorAll('.needs-validation');
+
+            Array.prototype.slice.call(forms).forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (!form.checkValidity()) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+
+                    form.classList.add('was-validated');
+                    setWasValidated(true);
+                }, false);
+            });
+        };
+
+        initializeValidation();
+    }, []);
 
     useEffect(() => {
         validateEmail();
@@ -29,6 +51,21 @@ const useFormValidation = () => {
         setIsEmailValid(pattern.test(email));
     };
 
+    function resetFields() {
+        setName("");
+        setAddress("");
+        setEmail("");
+        setPhone("");
+        setWasValidated(false);
+        setIsEmailValid(false);
+        setIsValid(false);
+        const forms = document.querySelectorAll('.needs-validation');
+
+        Array.prototype.slice.call(forms).forEach(function (form) {
+            form.classList.remove('was-validated');
+        }, false);
+    }
+
     return {
         name,
         setName,
@@ -39,7 +76,9 @@ const useFormValidation = () => {
         address,
         setAddress,
         isEmailValid,
-        isValid
+        isValid,
+        resetFields,
+        wasValidated,
     };
 };
 
