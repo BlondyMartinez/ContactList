7975d9f8806ce8a73094105000f0bb7a7cad181d
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-15 05:47:00 +0000

    ability to create new user

---
+++ src/js/component/new_user.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useContext } from "react";
+import { Context } from "../store/appContext";
+
+const NewUser = () => {
+    const { store, actions } = useContext(Context);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const [username, setUsername] = useState('');
+    const [validUsername, setValidUsername] = useState(null);
+
+    const handleChange = (event) => {
+        const username = event.target.value.trim();
+        setUsername(username);
+        validateUsername(username);
+    };
+
+    function validateUsername(username) {
+        const isUsernameTaken = store.users.some(user => user.slug === username);
+        setValidUsername(!isUsernameTaken && username.length > 0);
+    }
+
+
+    return (
+        <div className="mt-2 card">
+            <button className="btn btn-success w-100" onClick={() => setIsOpen(!isOpen)}>New User</button>
+            { isOpen &&
+                <div className="p-2">
+                    <h2 className="text-center">Create user</h2>
+                    <div className="mb-3">
+                        <label htmlFor="username" className="form-label">Username</label>
+                        <input type="text" className="form-control" id="username" placeholder="Enter username" value={username} onChange={handleChange} required/>
+                    </div>
+                    <button className="btn btn-success" onClick={() => { if(validUsername) actions.createUser(username); }}>Save</button>
+                </div>
+            }
+	    </div>
+    );
+};
+
+export default NewUser;
--- src/js/views/change_user.js
+++ src/js/views/change_user.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import UserList from "../component/user_list.jsx"; 
+import NewUser from "../component/new_user.jsx";
 
 export const ChangeUser = () => (
 	<div className="container">
 		<div className="row d-flex flex-column align-items-center">
 			<div className="col-sm-12 col-md-8 col-lg-6">
 				<UserList></UserList>
-			<Link to="/">
-				<span>get back to contacts</span>
-			</Link>
+				<NewUser></NewUser>
+				<Link to="/">
+					<span>get back to contacts</span>
+				</Link>
 			</div>
 		</div>
 	</div>
