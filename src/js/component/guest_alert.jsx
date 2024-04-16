737f1169c30a98bac56cd0fdef7cf69f3bdb4977
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-16 14:58:59 +0000

    increased width on big screens

--- src/js/component/guest_alert.jsx
+++ src/js/component/guest_alert.jsx
@@ -10,7 +10,7 @@ const GuestAlert = () => {
         store.user.id === "guest" && (
             <div className="container">
                 <div className="row d-flex justify-content-center">
-                    <div className={`col-sm-11 col-md-8 ${location.pathname === "/" ? "col-lg-8" : "col-lg-6"}  m-2 mb-0`}>
+                    <div className={`col-sm-11 col-md-8 ${location.pathname === "/" ? "col-lg-10" : "col-lg-6"}  m-2 mb-0`}>
                         <div className="alert alert-danger">
                             In guest mode, any contacts added will be temporary and will not persist when the page is refreshed or when the user switches to a different account.
                         </div>
