6d433b204fa70e2849183482645b47e3224560fa
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-16 11:12:18 +0000

    guest_alert now uses location to determine its width

--- src/js/component/guest_alert.jsx
+++ src/js/component/guest_alert.jsx
@@ -1,13 +1,16 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import { useLocation } from 'react-router-dom';
 
 const GuestAlert = () => {
     const { store } = useContext(Context);
+    const location = useLocation();
+
     return (
         store.user.id === "guest" && (
             <div className="container">
                 <div className="row d-flex justify-content-center">
-                    <div className=" col-sm-11 col-md-8 col-lg-6 m-2 mb-0">
+                    <div className={`col-sm-11 col-md-8 ${location.pathname === "/" ? "col-lg-8" : "col-lg-6"}  m-2 mb-0`}>
                         <div className="alert alert-danger">
                             In guest mode, any contacts added will be temporary and will not persist when the page is refreshed or when the user switches to a different account.
                         </div>
